a0292bc9b413562115e44b7bf78afa031e4230a7
Glitch (glitch-hello-react) <none>
2022-10-30 01:55:54 +0000

    💺📠 Checkpoint ./vite.config.js:49461564/127 ./package.json:49461564/82

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: true
+    strictPort: true,
+    hmr: {
+      port: 443 // Run the websocket server on the SSL port
+    }
   } 
 });
