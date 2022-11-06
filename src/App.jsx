27302c61291d0ae8d7b57e2427421509ac88fefc
B-Norm <bkbrad1220@gmail.com>
2022-11-05 21:39:49 -0500

    registration page is connected and logic is working for entering values. Need to work on onFinish function for creating a new user and checking if email is taken

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import Login from './pages/login/login.jsx';
-import Register from "./pages/login/register.jsx";
+import React from "react";
+import Login from "./pages/login/login.jsx";
+import Register from "./pages/register/register.jsx";
 
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-//import PageRouter from './components/router.jsx'
+import PageRouter from "./components/router.jsx";
 
 function App() {
-  
   return (
     <Router>
-      <Routes>
-        <Route path="/" element ={<Login />}/>
-        
-        <Route path="/register" element= {<Register />} />
-      </Routes>
+      <PageRouter />
     </Router>
   );
 }
